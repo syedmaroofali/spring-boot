178ddb70595cdf4e29b13b3e5132b97dd2312f2e
syedmaroofali <40570230+syedmaroofali@users.noreply.github.com>
2020-09-11 10:15:51 +0000

    Update Main.java
    refactor code

--- spring-boot-actuator/src/main/java/com/javacodegeeks/example/Main.java
+++ spring-boot-actuator/src/main/java/com/javacodegeeks/example/Main.java
@@ -2,12 +2,27 @@ package com.javacodegeeks.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/status")
 @SpringBootApplication
 public class Main {
+
+      @GetMapping
+
+    public String getstatus() {
+
+        return ("Your App is Up and Running");
+
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
+           
 
 }
+
